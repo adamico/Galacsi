e9c548def2710dccd8d229f4b6734eff334193d0
Andrea <andrea.damico@gmail.com>
2009-12-02 17:36:31 +0100

    added some TODOs

--- app/controllers/produits_controller.rb
+++ app/controllers/produits_controller.rb
@@ -4,16 +4,22 @@ class ProduitsController < ApplicationController
   filter_resource_access :additional_collection =>  [:nonvalidated, :index],
       :additional_member => {:validate => :update}
 
+  #TODO créer une action pour mettre à jour l'attribute "date_validation" avec la date du jour
+  #TODO créer une action pour montrer la liste des molécules non vérifiées depuis une certaine date
+
+  #TODO créer une action pour passer à l'étape de validation suivante
   def validate
     @produit.update_attribute :validation, 1
     flash[:notice] = "Le produit a été validé."
     redirect_to nonvalidated_produits_path
   end
 
+  #TODO modifier validation= la valeur correspondante à l'étape "à valider"
   def nonvalidated
     @produits = Produit.all(:conditions => "validation = 0", :order => "name ASC")
   end
 
+  #TODO modifier validation= la valeur correspondante à l'étape "validé"
   def index
     @search = Produit.search(params[:search])
     @produits = @search.all(:conditions => "validation = 1")
--- app/models/produit.rb
+++ app/models/produit.rb
@@ -1,4 +1,9 @@
 class Produit < ActiveRecord::Base
   attr_accessible :name
+
+  #TODO créer un attribute "date_de_validation" qui correspond à la date à laquelle les infos ont été révérifiées (avec ou sans modifications)
+  #TODO créer soit un lookup avec les type de validations, soit une association
+
+  #TODO rajouter 3 niveaux de validation supplémentaires (initialisé, brouillon, gelé)
   attr_accessible :validation
 end
