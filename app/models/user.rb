a63016343cee7dbd442b950d96eab060bf32f104
Andrea <andrea.damico@gmail.com>
2009-12-02 17:42:27 +0100

    removed a wrong TODO in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ActiveRecord::Base
   has_many :assignments
   has_many :roles, :through => :assignments
 
-  #TODO créer un attribute "date_de_validation" qui correspond à la date à laquelle les infos ont été révérifiées (avec ou sans modifications)
   def role_symbols
     roles.map do |role|
       role.name.underscore.to_sym
