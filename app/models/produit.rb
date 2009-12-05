f2129b667889522716f39d1a4fea8b936f1be17a
Andrea <andrea.damico@gmail.com>
2009-12-05 11:14:02 +0100

    added validation_reflection plugin

--- app/models/produit.rb
+++ app/models/produit.rb
@@ -1,10 +1,10 @@
 class Produit < ActiveRecord::Base
+  validates_presence_of :name
+  validates_uniqueness_of :name
+
   include AASM
   #TODO créer un attribute "date_de_validation" qui correspond à la date à laquelle les infos ont été révérifiées (avec ou sans modifications)
-  #TODO créer soit un lookup avec les type de validations, soit une association
 
-  # 0 => brouillon, 1 => à_valider, 2 => validé, 3 => gelé)
-  attr_accessible :validation
   attr_accessible :name
 
   # AASM stuff
