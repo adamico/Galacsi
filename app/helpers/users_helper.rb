2ed4cba49b9f2ffd14fc0bcb38d7999d1c7cce1b
Andrea <andrea.damico@gmail.com>
2009-12-09 10:46:32 +0100

    refactored users view helper for haml

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,13 @@
 module UsersHelper
-  #TODO refactor roles_checkbox helper method to haml
   def roles_checkbox
     roles = has_role?(:admin) ? Role.all : Role.all[1..-1]
-    html = "<ul>"
-    for role in roles
-      html << '<li>'
-      html << check_box_tag("user[role_ids][]", role.id, @user.roles.include?(role))
-      html << label_tag("#{role.name}")
-      html << '</li>'
-    end
-    html << "</ul>"
-    html
+    haml_tag :ul do
+      for role in roles
+        haml_tag :li do
+          haml_concat(check_box_tag("user[role_ids][]", role.id, @user.roles.include?(role)))
+          haml_concat(label_tag("#{role.name}"))
+        end#li
+      end#for
+    end#ul
   end
 end
