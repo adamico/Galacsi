c4c202f690ffd14ca3ac768c00cc9e2ab7b4a63d
Andrea <andrea.damico@gmail.com>
2010-02-10 14:22:18 +0100

    added bundler support

--- test/unit/fiche_test.rb
+++ test/unit/fiche_test.rb
@@ -6,3 +6,18 @@ class FicheTest < ActiveSupport::TestCase
     assert true
   end
 end
+
+# == Schema Information
+#
+# Table name: fiches
+#
+#  id              :integer         not null, primary key
+#  name            :string(255)
+#  created_at      :datetime
+#  updated_at      :datetime
+#  state           :string(255)
+#  decision_id     :integer
+#  validation_date :date
+#  commentaire     :text
+#
+
