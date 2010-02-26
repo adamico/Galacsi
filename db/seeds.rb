3d9b0973c7fd1f7d0b6ff429fb8e3b27f7476e1e
Andrea <andrea.damico@gmail.com>
2010-02-26 16:58:59 +0100

    added seeds, tweaked css

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,9 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+#
+
+decisions = [ "contre-indiqué", "à discuter", "compatible", "sous conditions"]
+decisions.each do |d|
+  Decision.create(:name => d)
+end
