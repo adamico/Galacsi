cf6413a580bafd014dc556c5257004411ca38acf
Andrea <andrea.damico@gmail.com>
2010-03-13 19:41:06 +0100

    added function to toggle fiche details

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,8 @@ decisions.each_with_index do |d, i|
   Decision.create(:name => d, :abbr => abbrev[i])
 end
 
+Distinction.create([{:name => "indication"}, {:name => "voie"}, {:name => "dosage"}])
+
 classes = [ "Analgésique - Antiinflammatoire", "Anesthésie et réanimation", "Antibiotique et Antibactérien", "Antiparasitaire", "Antiviral", "Cancérologie", "Cardiologie et angiologie", "Dermatologie", "Endocrinologie - Troubles Métaboliques", "Gastro-entéro-hépatologie", "Gynécologie-obstétrique", "Hemostase", "Immuno-allergologie", "Inclassables", "Neurologie", "Ophtalmologie", "Oto-rhino-laryngologie", "Phytothérapie", "Pneumologie", "Produit de contraste", "Psychiatrie - Antidépresseur", "Psychiatrie - Anxiolytique", "Psychiatrie - Hypnotique", "Psychiatrie - Neuroleptique", "Rhumatologie", "Substitution", "Urologie", "Vaccin"]
 
 classes.each do |c|
