4b89f710d72f8a77a99ff4e3a8fc6735e7638caf
Andrea <andrea.damico@gmail.com>
2010-07-20 10:20:38 +0200

    temporarily disabled recaptcha, not working on localhost through http_proxy

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,3 @@
+# recaptcha keys for galacsi.heroku.com
+#ENV['RECAPTCHA_PUBLIC_KEY']  = '6LfFmLsSAAAAAMZDjXeNzOIDPL3YyD82rpffGbzk'
+#ENV['RECAPTCHA_PRIVATE_KEY'] = '6LfFmLsSAAAAAMYsNAe1wvq-f_7l3jzEDhtJp91E'
