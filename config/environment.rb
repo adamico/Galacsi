2106e7da1aa3255591d19a38fc7798b3b434ffe2
Andrea <andrea.damico@gmail.com>
2010-07-20 11:50:04 +0200

    added checking if dci has any valide fiches and a message

--- config/environment.rb
+++ config/environment.rb
@@ -50,5 +50,3 @@ Rails::Initializer.run do |config|
   config.i18n.load_path += Dir[File.join(RAILS_ROOT, 'config', 'locales', '**', '*.{rb,yml}')]
   config.i18n.default_locale = :fr
 end
-ENV['RECAPTCHA_PUBLIC_KEY']  = '6LfFmLsSAAAAAMZDjXeNzOIDPL3YyD82rpffGbzk'
-ENV['RECAPTCHA_PRIVATE_KEY'] = '6LfFmLsSAAAAAMYsNAe1wvq-f_7l3jzEDhtJp91E'
