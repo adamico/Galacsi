4cdcb9f7cefbb0e8a33aa109c7933073e3f14a61
Andrea <andrea.damico@gmail.com>
2010-09-20 14:48:28 +0200

    dci specs

--- spec/models/dci_spec.rb
+++ spec/models/dci_spec.rb
@@ -1,23 +1,51 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+#encoding: utf-8
+require 'spec_helper'
 
 describe Dci do
-  before(:each) do
-    @dci = Factory.build(:dci)
-  end
-  it "should be valid" do
-    dci = Factory.build(:dci, :name => "value for name")
-    dci.should be_valid
-  end
+  let(:dci) {Factory.build(:dci)}
+  subject {dci}
+
+  it {should be_valid}
+
   describe "#commercial_names" do
+    it "should exist" do
+      subject.should respond_to(:commercial_names)
+    end
     it "should list the associated specialites names" do
       spec1 = Factory(:specialite, :name => "spec1")
       spec2 = Factory(:specialite, :name => "spec2")
-      dci = Factory(:dci, :specialites => [spec1, spec2])
-      dci.commercial_names.should == "Spec1, Spec2"
+      subject.specialites = [spec1, spec2]
+      subject.commercial_names.should == "Spec1, Spec2"
     end
   end
-  describe "#assign_commercial_names" do
-    it "should assign existing specialites or create them"
+  describe "when saved" do
+    it "should assign associated specialites using #commercial_names" do
+      com_names = "cn1, cn2, cn3"
+      subject.commercial_names = com_names
+      subject.save!
+      specialites = com_names.split(', ').map do |name|
+        Specialite.find_by_name(name)
+      end
+      subject.specialites.should == specialites
+    end
+    it "should set the unicode stripped name" do
+      subject.name = "époque"
+      subject.save!
+      subject.stripped_name.should == "epoque"
+    end
+  end
+  describe "#classes_therapeutiques" do
+    it "should exist" do
+      subject.should respond_to(:classes_therapeutiques)
+    end
+    it "should list the associated c.t." do
+      cts = []
+      3.times do |i|
+        cts << Factory(:classe_therapeutique, :name => "ct#{i}")
+      end
+      subject.classe_therapeutiques = cts
+      subject.classes_therapeutiques.should == "Ct0, Ct1, Ct2"
+    end
   end
 end
 
