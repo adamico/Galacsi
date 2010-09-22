d96810ed5531b84172472b05268fc52b86d637e3
Andrea <andrea.damico@gmail.com>
2010-09-22 17:18:27 +0200

    refactored seeds, preparing to reset db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,47 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
 #
+#
 
-decisions = [ "contre-indiqué", "à discuter", "compatible", "sous conditions"]
-abbrev = %w[ci ad comp sc]
-Decision.delete_all
-decisions.each_with_index do |d, i|
-  Decision.create(:name => d, :abbr => abbrev[i])
+seed_decisions
+seed_distinctions
+seed_classes
+#seed_dcis
+#seed_fiches
+
+def seed_decisions
+  decisions = [ "contre-indiqué", "à discuter", "compatible", "sous conditions"]
+  abbrev = %w[ci ad comp sc]
+
+  decisions.each_with_index do |d, i|
+    Decision.create(:name => d, :abbr => abbrev[i])
+  end
 end
 
-Distinction.create([{:name => "indication"}, {:name => "voie"}, {:name => "dosage"}])
 
-classes = [ "Analgésique - Antiinflammatoire", "Anesthésie et réanimation", "Antibiotique et Antibactérien", "Antiparasitaire", "Antiviral", "Cancérologie", "Cardiologie et angiologie", "Dermatologie", "Endocrinologie - Troubles Métaboliques", "Gastro-entéro-hépatologie", "Gynécologie-obstétrique", "Hemostase", "Immuno-allergologie", "Inclassables", "Neurologie", "Ophtalmologie", "Oto-rhino-laryngologie", "Phytothérapie", "Pneumologie", "Produit de contraste", "Psychiatrie - Antidépresseur", "Psychiatrie - Anxiolytique", "Psychiatrie - Hypnotique", "Psychiatrie - Neuroleptique", "Rhumatologie", "Substitution", "Urologie", "Vaccin"]
+def seed_distinctions
+  distinctions = [
+    "indication",
+    "voie",
+    "dosage"
+  ]
+  distinctions.each do |dist|
+    Distinction.create(:name => dist)
+  end
+end
 
-classes.each do |c|
-  ClasseTherapeutique.find_or_create_by_name(c)
+def seed_classes
+  classes = [ "Analgésique - Antiinflammatoire", "Anesthésie et réanimation", "Antibiotique et Antibactérien", "Antiparasitaire", "Antiviral", "Cancérologie", "Cardiologie et angiologie", "Dermatologie", "Endocrinologie - Troubles Métaboliques", "Gastro-entéro-hépatologie", "Gynécologie-obstétrique", "Hemostase", "Immuno-allergologie", "Inclassables", "Neurologie", "Ophtalmologie", "Oto-rhino-laryngologie", "Phytothérapie", "Pneumologie", "Produit de contraste", "Psychiatrie - Antidépresseur", "Psychiatrie - Anxiolytique", "Psychiatrie - Hypnotique", "Psychiatrie - Neuroleptique", "Rhumatologie", "Substitution", "Urologie", "Vaccin"]
+
+  classes.each do |c|
+    ClasseTherapeutique.create(:name => c)
+  end
+end
+
+def seed_dcis
+  
+end
+
+def seed_fiches
+  
 end
