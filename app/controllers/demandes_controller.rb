39500186f2215b3cc1f964150519c84d78cf7431
Andrea <andrea.damico@gmail.com>
2010-09-23 10:30:00 +0200

    reverting recaptcha gem, adding raw to recaptcha helper

--- app/controllers/demandes_controller.rb
+++ app/controllers/demandes_controller.rb
@@ -16,7 +16,8 @@ class DemandesController < ApplicationController
   def create
     @demande = Demande.new(params[:demande])
     if verify_recaptcha(
-      :model => @demande) && @demande.save
+      :model => @demande,
+      :message => "Veuillez recopier les 2 mots dans le champ de vérification") && @demande.save
     #if @demande.save
       flash[:notice] = "La demande a été enregistrée."
       redirect_to root_path
