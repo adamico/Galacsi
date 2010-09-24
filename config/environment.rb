4718b6d157fc828ceb16655c8d9a4f3b150ed26e
Andrea <andrea.damico@gmail.com>
2010-09-24 12:28:33 +0200

    adding rails version 2.3.9 in env

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-#RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.9' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -10,13 +10,13 @@ Rails::Initializer.run do |config|
   config.gem "haml"
   config.gem 'RedCloth', :lib => 'redcloth'
   config.gem "searchlogic"
-  config.gem 'state_machine'
-  config.gem 'devise', :version => '1.0.7'
+  config.gem "state_machine"
+  config.gem "devise', :version => '1.0.7"
   config.gem "cancan"
   config.gem "formtastic"
   config.gem "activesupport"
-  config.gem "sqlite3-ruby", :lib => 'sqlite3'
   config.gem "friendly_id", :version => ">= 2.3"
+  config.gem "will_paginate"
   config.gem "recaptcha", :lib => "recaptcha/rails"
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
