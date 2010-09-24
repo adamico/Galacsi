20745e9a3dfa3525e0f1ed40c50663de79d2ff44
Andrea <andrea.damico@gmail.com>
2010-09-24 11:48:20 +0200

    removed old seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,3 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
 #
-
-decisions = [ "contre-indiqué", "à discuter", "compatible", "sous conditions"]
-abbrev = %w[ci ad comp sc]
-Decision.delete_all
-decisions.each_with_index do |d, i|
-  Decision.create(:name => d, :abbr => abbrev[i])
-end
-
-Distinction.create([{:name => "indication"}, {:name => "voie"}, {:name => "dosage"}])
-
-classes = [ "Analgésique - Antiinflammatoire", "Anesthésie et réanimation", "Antibiotique et Antibactérien", "Antiparasitaire", "Antiviral", "Cancérologie", "Cardiologie et angiologie", "Dermatologie", "Endocrinologie - Troubles Métaboliques", "Gastro-entéro-hépatologie", "Gynécologie-obstétrique", "Hemostase", "Immuno-allergologie", "Inclassables", "Neurologie", "Ophtalmologie", "Oto-rhino-laryngologie", "Phytothérapie", "Pneumologie", "Produit de contraste", "Psychiatrie - Antidépresseur", "Psychiatrie - Anxiolytique", "Psychiatrie - Hypnotique", "Psychiatrie - Neuroleptique", "Rhumatologie", "Substitution", "Urologie", "Vaccin"]
-
-classes.each do |c|
-  ClasseTherapeutique.find_or_create_by_name(c)
-end
