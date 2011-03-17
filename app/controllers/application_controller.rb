21c51baf48415fcc2e2c0e4096696c11018fc8b8
Andrea <andrea.damico@gmail.com>
2011-03-17 15:45:46 +0100

    another rails-best-practice

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,15 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
+  before_filter :find_expired_fiches
+  before_filter :find_pages
+  before_filter :find_dcis_with_recent_fiches
+
   rescue_from CanCan::AccessDenied do |exception|
     exception.default_message = "Vous n'êtes pas authorisé à voir cette page, veuillez vous connecter!"
     flash[:error] = exception.message
     redirect_to new_user_session_path
   end
-
-  before_filter :find_expired_fiches
-  before_filter :find_pages
 
   private
 
@@ -23,5 +24,9 @@ class ApplicationController < ActionController::Base
 
   def find_pages
     @pages_without_home = Page.without_home
+  end
+
+  def find_dcis_with_recent_fiches
+    @dcis_with_recent_fiches = Dci.with_recent_fiches
   end
 end
