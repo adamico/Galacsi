52ad205721cd8f983ae7fc5edbe8ea7aa134206b
Andrea D'Amico <andrea.damico@gmail.com>
2013-02-25 14:59:19 +0100

    fixing various config for rails 3.2

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,12 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-# If you have a Gemfile, require the gems listed there, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env) if defined?(Bundler)
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require *Rails.groups(:assets => %w(development test))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
 
 module Galacsi
   class Application < Rails::Application
@@ -31,14 +34,18 @@ module Galacsi
     # config.i18n.default_locale = :de
     config.i18n.default_locale = :fr
 
-    # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w(https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.6/jquery-ui.min.js rails)
-
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
 
     if Rails.env.production?
       config.middleware.use("Rack::GoogleAnalytics", :web_property_id => "UA-20156445-3")
