6f4e0f3fec4d9d98d819d8acc54af585f2d90f59
Andrea D'Amico <andrea.damico@gmail.com>
2013-03-07 14:35:30 +0100

    made action buttons smaller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,9 +19,9 @@ module ApplicationHelper
            when Fiche; [object.dci, object]
            else object;
            end
-    buttons.push link_to("Voir #{object.class.name}", polymorphic_path(path), class: "btn btn-success") if can? :show, object and [Fiche, Demande].include?(object.class)
-    buttons.push link_to("Modifier #{object.class.name}", edit_polymorphic_path(object), class: "btn btn-warning") if can? :update, object
-    buttons.push link_to("Détruire #{object.class.name}", polymorphic_path(object), confirm: 'Etes-vous sûr ?', method: :delete, class: "btn btn-danger ") if can? :destroy, object
+    buttons.push link_to("Voir #{object.class.name}", polymorphic_path(path), class: "btn btn-small btn-success") if can? :show, object and [Fiche, Demande].include?(object.class)
+    buttons.push link_to("Modifier #{object.class.name}", edit_polymorphic_path(object), class: "btn btn-small btn-warning") if can? :update, object
+    buttons.push link_to("Détruire #{object.class.name}", polymorphic_path(object), data: {confirm: 'Etes-vous sûr ?'}, method: :delete, class: "btn btn-small btn-danger ") if can? :destroy, object
     content = buttons.join("\n").html_safe
     content_tag :div, content, class: "btn-group"
   end
