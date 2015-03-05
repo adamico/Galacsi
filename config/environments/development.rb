bf1808f57653621b6a38db129a63641e44aa916c
Andrea D'Amico <andrea.damico@gmail.com>
2015-03-05 12:33:04 +0100

    update rails config files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,9 @@
 Galacsi::Application.configure do
-  # Settings specified here will take precedence over those in config/environment.rb
+  # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
   # every request.  This slows down response time but is perfect for development
-  # since you don't have to restart the webserver when you make code changes.
+  # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
   # Log error messages when you accidentally call methods on nil.
@@ -16,8 +16,6 @@ Galacsi::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
-  # this is for devise, set to real address in production
-  config.action_mailer.default_url_options = {:host => 'localhost:3000'}
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
@@ -29,4 +27,7 @@ Galacsi::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # this is for devise, set to real address in production
+  config.action_mailer.default_url_options = {:host => 'localhost:3000'}
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-Galacsi::Application.config.session_store :cookie_store, :key => '_galacsi_new_session'
+Galacsi::Application.config.session_store :cookie_store, key: '_galacsi_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
+# (create the session table with "rails generate session_migration")
 # Galacsi::Application.config.session_store :active_record_store
