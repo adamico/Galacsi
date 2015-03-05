ea062ee85510c4b467848d24eaa623612224f78a
Andrea D'Amico <andrea.damico@gmail.com>
2015-03-05 13:56:32 +0100

    updated rails config

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
 Galacsi::Application.config.secret_token = 'd5308d189363d22d4d6a83dec4ec269ec73f0a8eda8d45024c72e3047171c12312850ea4d4ef9fd71f4f78b72440a073b191db07de2d0b4a7951e5d34a221375'
+Galacsi::Application.config.secret_key_base = '59f24f5393e868eb5bbc22933b3e27b548fa38f0c38ee338951677c00e1b7422b55a5aec8efa9b3be46a6a4a0d63d93ed46033b2aeb893fd422f862c24d33405'
